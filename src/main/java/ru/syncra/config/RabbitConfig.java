f7995ddb40195bff12ae3dfc66f1ebb61876035d
deus <deus@korul.com>
2025-03-17 15:27:39 +0300

    main: fix json mapping

--- src/main/java/ru/syncra/config/RabbitConfig.java
+++ src/main/java/ru/syncra/config/RabbitConfig.java
@@ -4,11 +4,14 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.DefaultClassMapper;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
 
 @Configuration
 public class RabbitConfig {
@@ -64,7 +67,13 @@ public class RabbitConfig {
 
     @Bean
     public MessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
+        Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter();
+        converter.setClassMapper(new DefaultClassMapper() {
+            {
+                setDefaultType(Map.class);
+            }
+        });
+        return converter;
     }
 
     @Bean
